efdd51efb7c7c43e523700288af8cc5a0dd621ac
mihawkcola <zockerelf@gmail.com>
2023-03-28 15:53:26 +0200

    init controller(user and article)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ArticleController } from './article/article.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot(), UserModule],
+  controllers: [AppController, UserController, ArticleController],
   providers: [AppService],
 })
 export class AppModule {}
