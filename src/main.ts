baa7d9b55dadff355c2d821bdd555ed6f970cc9c
mihawkcola <zockerelf@gmail.com>
2023-03-28 15:23:03 +0200

    add package env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.Port || 3000);
+  Logger.log(`Run on ${(await app.getUrl()).toString()}`);
 }
 bootstrap();
